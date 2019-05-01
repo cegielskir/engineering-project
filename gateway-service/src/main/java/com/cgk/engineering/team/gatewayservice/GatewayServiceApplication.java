140aa469a8a11fdb4bfbb0b1008999df8c47e047
Rafał Cegielski <cegielski.rafaal@gmail.com>
2019-05-01 23:09:10 +0200

    Setup services updating

--- gateway-service/src/main/java/com/cgk/engineering/team/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/cgk/engineering/team/gatewayservice/GatewayServiceApplication.java
@@ -2,8 +2,12 @@ package com.cgk.engineering.team.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableSwagger2
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
