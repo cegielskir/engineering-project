8c75c77c3a25a96dbcacfeceff97615691d4a2b3
Rafał Cegielski <cegielski.rafaal@gmail.com>
2019-05-01 20:50:13 +0200

    Cloud structure adding - continuation

---
+++ config-service/src/main/java/com/cgk/engineering/team/configservice/ConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cgk.engineering.team.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiceApplication.class, args);
+	}
+
+}
