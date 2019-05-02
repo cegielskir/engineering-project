25675232935b840d43533c8c1e217db6f286aa8b
Rafał Cegielski <cegielski.rafaal@gmail.com>
2019-05-02 19:06:29 +0200

    Many changes making

--- gateway-service/src/main/java/com/cgk/engineering/team/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/cgk/engineering/team/gatewayservice/GatewayServiceApplication.java
@@ -3,10 +3,12 @@ package com.cgk.engineering.team.gatewayservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableZuulProxy
 @EnableSwagger2
 public class GatewayServiceApplication {
 
---
+++ main-service/src/main/java/com/cgk/engineering/team/mainservice/controller/ArticleController.java
@@ -0,0 +1,31 @@
+package com.cgk.engineering.team.mainservice.controller;
+
+
+import com.cgk.engineering.team.mainservice.model.Article;
+import com.cgk.engineering.team.mainservice.repository.ArticleRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ArticleController {
+
+    @Autowired
+    ArticleRepository articleRepository;
+
+    @GetMapping("/core/articles/{articleId}")
+    public Article getArticle(@PathVariable ObjectId articleId){
+        System.out.println(articleRepository.findAll());
+        System.out.println(articleId);
+        return articleRepository.findBy_id(articleId);
+    }
+
+    @PostMapping("/core/articles")
+    public Article addArticle(@RequestBody Article article){
+        System.out.println(article);
+        articleRepository.save(article);
+        return article;
+    }
+
+
+}
