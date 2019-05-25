e3614c9e770c23b4d82800aa474b2ac993d1a5cd
Paweł Gędłek <pawelgedlek@gmail.com>
2019-05-25 15:55:18 +0200

    updatong cors policy

--- simple-client/src/main/java/com/cgk/engineering/team/simpleclient/controller/AlgorithmController.java
+++ simple-client/src/main/java/com/cgk/engineering/team/simpleclient/controller/AlgorithmController.java
@@ -6,12 +6,10 @@ import com.cgk.engineering.team.simpleclient.model.Comparison;
 import com.cgk.engineering.team.simpleclient.repository.ArticleRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping(value = "/algorithm")
 public class AlgorithmController {
 
