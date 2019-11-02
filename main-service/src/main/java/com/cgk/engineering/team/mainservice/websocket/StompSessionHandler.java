70510ce632ef58b02175eab617476241a4e97435
Rafał Cegielski <cegielski.rafaal@gmail.com>
2019-11-02 18:23:25 +0100

    FIxing websockets

--- main-service/src/main/java/com/cgk/engineering/team/mainservice/websocket/StompSessionHandler.java
+++ main-service/src/main/java/com/cgk/engineering/team/mainservice/websocket/StompSessionHandler.java
@@ -28,17 +28,22 @@ public class StompSessionHandler extends StompSessionHandlerAdapter {
 
     @Override
     public void handleFrame(StompHeaders headers, Object payload) {
-        Runnable runnable =
-            () -> {
-                ComparisonData comparisonData = (ComparisonData) payload;
-                comparisonData.setMetric("Dice"); //USUN TO!
-                comparisonWebSocketController.sendComparison(algorithmClient.getComparisonWithChosenMetric(comparisonData));
-            };
-        new Thread(runnable).start();
+        new Thread(new FrameHandlerTask(payload)).start();
     }
 
     public void initMessage(ObjectId articleId, StompSession session){
         session.subscribe("/article-db", this);
         session.send("/db/article/" + articleId, "");
+    }
+
+    class FrameHandlerTask implements Runnable {
+        Object payload;
+
+        FrameHandlerTask(Object payload) { this.payload = payload; }
+        public void run() {
+            ComparisonData comparisonData = (ComparisonData) payload;
+            comparisonData.setMetric("Dice"); //USUN TO!
+            comparisonWebSocketController.sendComparison(algorithmClient.getComparisonWithChosenMetric(comparisonData));
+        }
     }
 }
